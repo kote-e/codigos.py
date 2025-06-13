27810f7e09b0bb153ec3b352bd97ebbadd09d1fa
kote-e <mjosesanm@gmail.com>
2025-06-12 23:31:20 -0400

    actualizar clases

---
+++ clases_python/clase4.py
@@ -0,0 +1,16 @@
+## Ejemplo de función en Python
+def saludar(nombre):
+    mensaje = "Hola, " + nombre + "!"
+    return mensaje
+
+print(saludar("nombre"))
+
+
+
+# Definición de una función que suma dos números
+def suma(a, b):
+    return a + b
+
+# Llamada a la función con argumentos
+resultado = suma(5, 7)
+print(resultado)  # Salida: 12

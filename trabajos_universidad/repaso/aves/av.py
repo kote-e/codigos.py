f0651fea04a1dd2cdf832b1382eaba1b723f0e89
kote-e <mjosesanm@gmail.com>
2025-06-24 22:09:27 -0400

    actualizar clases

--- trabajos_universidad/repaso/aves/av.py
+++ trabajos_universidad/repaso/aves/av.py
@@ -1,4 +1,4 @@
-def mas_vistis(nombre_archivo):
+def mas_vistos(nombre_archivo):
     archivo = open(nombre_archivo, "r")
     mas_vistos= open("mas_vistos.txt", "w")
     contador=0
@@ -20,13 +20,45 @@ def mas_vistis(nombre_archivo):
         mejores.append((aves[j], j))
     mejores.sort(reverse=True)
 
-
     for i in range(3):
         mas_vistos.write(f"se observaron {mejores[i][1]} especimenes de {mejores[i][0]}\n")
-    
+
     archivo.close()
     mas_vistos.close()
     return 0
 
 
-a= mas_vistis("observaciones.txt")
+
+def vistos_por_ciudad(nombre_archivo):
+    archivo = open(nombre_archivo, "r")
+    ciudades={}
+    mas_vistos_ciudad= open("mas_vistos_ciudad.txt", "w")
+    num_ciudades=0
+    for linea in archivo:
+        datos= linea.strip().split(";")
+        ciudad= datos[1]
+        pajaros= datos[-1].strip().split(",")
+        fecha = datos[0].strip().split("/")
+        if ciudad not in ciudades:
+            ciudades[ciudad]= []
+            for pa in pajaros:
+                ciudades[ciudad].append([fecha, pa])
+        else:
+            for pa in pajaros:
+                ciudades[ciudad].append([fecha, pa])
+    
+    for ciudad in ciudades:
+        datos= ciudades[ciudad]
+
+        num_ciudades+=1
+        
+            
+
+
+
+
+    archivo.close()
+    return a0
+
+
+a= mas_vistos("observaciones.txt")
